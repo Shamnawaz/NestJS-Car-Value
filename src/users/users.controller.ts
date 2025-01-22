2e5bfa00f842874ee352a20d309e95518b21d759
Shamnawaz <shamnawaz.subakhan.sio@gmail.com>
2025-01-22 11:34:35 +0100

    Refactor - Interceptor Implementation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,6 +14,7 @@ import {
 import { CreateUserDto } from './dtos/create-user-dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user-dto';
+import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
@@ -25,9 +26,10 @@ export class UsersController {
         this.userService.create(body.email, body.password);
     }
 
-    @UseInterceptors(ClassSerializerInterceptor)
+    @UseInterceptors(SerializeInterceptor)
     @Get('/:id')
     async findUser(@Param('id') id: string) {
+        console.log('handler is running');
         const user = await this.userService.findOne(+id)
         if(!user) {
             throw new NotFoundException('User not found');
