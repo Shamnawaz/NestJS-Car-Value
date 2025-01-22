9202fd20b067f78f66e000ac9781eebad59c5ce5
Shamnawaz <shamnawaz.subakhan.sio@gmail.com>
2025-01-22 10:50:26 +0100

    Route Handlers Implementation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { 
+    Body, 
+    Controller, 
+    Post, 
+    Get, 
+    Patch, 
+    Param, 
+    Query, 
+    NotFoundException,
+    Delete
+ } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user-dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user-dto';
 
 @Controller('auth')
 export class UsersController {
@@ -11,4 +22,30 @@ export class UsersController {
     createUser(@Body() body: CreateUserDto) {
         this.userService.create(body.email, body.password);
     }
+
+    @Get('/:id')
+    async findUser(@Param('id') id: string) {
+        const user = await this.userService.findOne(+id)
+        if(!user) {
+            throw new NotFoundException('User not found');
+        }
+
+        return user;
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.userService.find(email);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.userService.remove(+id);
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        return this.userService.update(+id, body);
+    }
+
 }
