7df694983a7420a648f2a1d7b5ab2c224367571f
Shamnawaz <shamnawaz.subakhan.sio@gmail.com>
2025-01-22 12:01:47 +0100

    Refactor-Interceptor

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -6,10 +6,11 @@ import {
 } from "@nestjs/common";
 import { Observable } from "rxjs";
 import { map } from "rxjs";
-import { plainToClass } from "class-transformer";
-import { UserDto } from "src/users/dtos/user-dto";
+import { plainToInstance } from "class-transformer";
 
 export class SerializeInterceptor implements NestInterceptor {
+    constructor(private dto: any) {}
+
     intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
         // Run something before a request  is handled
         // by the request handler
@@ -18,7 +19,7 @@ export class SerializeInterceptor implements NestInterceptor {
         return handler.handle().pipe(
             map((data: any) => {
                 // Run something before the response is sent out
-                return plainToClass(UserDto, data, {
+                return plainToInstance(this.dto, data, {
                     excludeExtraneousValues: true,
                 } );
             })
