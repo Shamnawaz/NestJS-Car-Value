f1d63891d9127377ba0f987dba6ff42621db7dad
Shamnawaz <shamnawaz.subakhan.sio@gmail.com>
2025-01-27 09:56:35 +0100

    Cookie + Session Implementation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
     Query, 
     NotFoundException,
     Delete,
+    Session
  } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user-dto';
 import { UsersService } from './users.service';
@@ -25,9 +26,28 @@ export class UsersController {
         private authService: AuthService
     ) {}
 
+    @Get('/whoami')
+    whoAmI(@Session() session: any) {
+        return this.userService.findOne(session.id);
+    }
+
+    @Post('/signout')
+    signOut(@Session() session: any) {
+        session.id = null;
+    }
+
     @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {
-        return this.authService.signup(body.email, body.password);
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signup(body.email, body.password);
+        session.id = user.id;
+        return user;
+    }
+
+    @Post('/signin')
+    async signin(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);
+        session.id = user.id
+        return user;
     }
 
     @Get('/:id')
