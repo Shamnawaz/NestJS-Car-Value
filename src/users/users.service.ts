cf23b7361969f43d9ca9ee87cd71b74b44964727
Shamnawaz <shamnawaz.subakhan.sio@gmail.com>
2025-01-27 10:02:00 +0100

    Fixup - findOne Method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,6 +14,9 @@ export class UsersService {
     }
 
     findOne(id: number) {
+        if(!id) {
+            throw new NotFoundException('Id not found');
+        }
         return this.repo.findOne({ where: { id } });
     }
 
